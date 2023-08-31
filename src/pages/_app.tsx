1953eec01f02dafe4d731920968222e1c464f38a
annamarcomini <annajuliamarcomini23@gmail.com>
2023-08-30 22:06:27 -0300

    retorno da função da api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,11 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { SidebarDrawerProvider } from "@/components/contexts/SidebarDrawerContext";
+import { makeServer } from "@/services/mirage";
+
+if (process.env.NODE_ENV === 'development'){ //verifica se o ambiente q roda a aplicação é o de desenvolvimento
+makeServer();
+}
 
 function MyApp({Component, pageProps}: AppProps) {
  return (
